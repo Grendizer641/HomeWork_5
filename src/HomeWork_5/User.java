1543b4c5f5fc1ed22f63c97ac04bc3a7abd5b38f
Grendizer <korensana@gmail.com>
2021-10-17 12:18:35 +0300

    Домашнее задание по 5 уроку

---
+++ src/HomeWork_5/User.java
@@ -0,0 +1,75 @@
+package HomeWork_5;
+
+public class User {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public User(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printUserInfo() {
+        System.out.println("ФИО: " + fullName + ", Должность: " + position + ", email: " + email + ", Телефон: " + phone
+                + ", Зарплата: " + salary + ", Возраст: " + age);
+        System.out.println();
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
